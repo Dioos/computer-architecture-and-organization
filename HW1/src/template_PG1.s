bfd44afa783f7e1b738e856773dfcfb19eb25b66
Dioos <jay8832791@gmail.com>
2019-10-02 00:08:46 +0800

    HW1-Calculation in assembly language

---
+++ HW1/src/template_PG1.s
@@ -0,0 +1,34 @@
+.data
+message: .asciiz "The answer is : "
+.text
+.globl main
+main:
+
+
+    addi $v0,$zero, 4   # print the character of the address pointer in the $a0
+    la $a0, message	   # set $a0  point to string in message
+    syscall
+
+
+	########################
+	# add your code here^^ #
+	########################
+	addi $t0, $t0, 3
+	addi $t1, $t1, 5
+	addi $t2, $t2, 9
+	addi $t3, $t3, 10
+	sub  $t0, $t0, $t1 
+	sub  $t2, $t2, $t3 
+	add  $t0, $t0, $t2
+	
+	
+
+	add $a0, $t0, $zero
+
+	
+end:
+	addi $v0,$zero, 1 # Sets $v0 to "1" to print $a0
+	syscall
+	addi $v0,$zero,10
+	syscall
+	
